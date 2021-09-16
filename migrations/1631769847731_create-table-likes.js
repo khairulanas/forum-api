335f90466efae4435f396cc53271b1ce508a782a
khairul anas <mafiakanan@gmail.com>
2021-09-16 12:33:06 +0700

    create table likes

---
+++ migrations/1631769847731_create-table-likes.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('likes', 'fk_likes.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('likes', 'fk_likes.thread_id_threads.id', 'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE');
+  pgm.addConstraint('likes', 'fk_likes.comment_id_comments.id', 'FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('likes');
+};
